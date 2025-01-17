0864335114f693fb3b447144ecb8fd07048c3f27
sgi0107 <sgi0107@osstem.com>
2025-01-17 15:53:52 +0900

    github page package 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header({ bride, groom }: HeaderProps) {
   return (
     <div className="header">
       <h1>{bride} & {groom}</h1>
-      <p className="subtitle">청첩장</p>
+      <p className="subtitle">♡청첩장♡</p>
     </div>
   );
 }
