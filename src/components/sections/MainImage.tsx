0dbdfdd0933e01c33390e75513a110318397ffe3
sgi0107 <rjsdlr4474@naver.com>
2025-02-12 07:37:26 +0900

    1) 모바일 환경에서 브라우즈 url 창 발생에 따른 화면 가변 시 MainImage Height 조정되게 수정 2) Layout 수평스크롤 방지 추가

--- src/components/sections/MainImage.tsx
+++ src/components/sections/MainImage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import styles from './MainImage.module.css';
 import mainImage from '../../assets/images/main-image.jpg';
 
@@ -8,9 +9,23 @@ type MainImageProps = {
   detailsText: string;
 };
 
-export function MainImage({ altText, mainText, coupleNames, detailsText}: MainImageProps) {
+export function MainImage({ altText, mainText, coupleNames, detailsText }: MainImageProps) {
+  
+  // 뷰포트 높이를 동적으로 설정하여 크기 변화 방지
+  useEffect(() => {
+    function updateVH() {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    
+    updateVH(); // 초기 실행
+    window.addEventListener('resize', updateVH);
+    
+    return () => window.removeEventListener('resize', updateVH);
+  }, []);
+
   return (
-    <div className={styles.mainImageContainer}>
+    <div className={styles.mainImageContainer} style={{ height: 'calc(var(--vh) * 100)' }}>
         <img src={mainImage} alt={altText} className={styles.mainImage} />
         <p className={styles.mainText}>{mainText}</p>
         <div className={styles.textOverlayContainer}>
